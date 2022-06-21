1a8eee3c6de7fe0ce349a14e38c1ef44771bbcb0
AlenaTrafimovich <trofimovich_elena@mail.ru>
2022-06-21 20:40:19 +0300

    fix4

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
         System.out.println("GIT is important for dev");
         System.out.println("GIT is CVS program");
         System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+
     }
 }
